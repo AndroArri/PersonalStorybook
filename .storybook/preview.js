1261fbe5c36503572bdd8421379ee95587c182e1
Andrea Arrighini <andrea.arrighini@ncoreplat.com>
2024-05-29 10:35:26 +0200

    Add configuration storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,45 @@
+import PrimeVue from "primevue/config";
+import { setup } from "@storybook/vue3";
+import Ripple from "primevue/ripple";
+import { themes } from "@storybook/theming";
+import { withThemeByClassName } from '@storybook/addon-themes';
+import Lara from "/resources/js/components/presets/Lara/";
+// Import global styles
+import "/resources/css/app.css";
+
+
+setup((app) => {
+    app.use(PrimeVue, {
+        unstyled: true,
+        pt: Lara,
+        ripple: true,
+    });
+    app.directive("ripple", Ripple);
+});
+/** @type { import('@storybook/vue3').Preview } */
+const preview = {
+    parameters: {
+        // Override the default dark theme
+        dark: { ...themes.dark, appBg: "black" },
+        // Override the default light theme
+        light: { ...themes.normal, appBg: "red" },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+};
+
+export const decorators = [
+    withThemeByClassName({
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ];
+
+export default preview;
