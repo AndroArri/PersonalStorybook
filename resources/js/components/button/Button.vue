eb051aacc2404f4456b2d860facd2f4aafaad48d
AndroArri <a.arrighini93@gmail.com>
2024-05-30 08:27:32 +0200

    Fix ButtonComponents

--- resources/js/components/button/Button.vue
+++ resources/js/components/button/Button.vue
@@ -1,70 +1,89 @@
 <template>
-    <Button
-        :id="props.id"
-        :severity="props.severity === 'primary' || link ? null : severity"
-        :size="sizeProxy"
-        :link="!props.link ? false : true"
-        :icon="props.icon"
-        :label="props.label"
-        :disabled="props.disabled"
-        :rounded="props.rounded"
-        :badge="props.badges"
-        @click="$emit('buttonClick', props.id)"
-    >
-        <slot></slot>
-    </Button>
+  <Button
+    :id="props.id"
+    :severity="severityProxy"
+    :size="sizeProxy"
+    :link="!props.link ? false : true"
+    :icon="props.icon"
+    :label="props.label"
+    :disabled="props.disabled"
+    :rounded="props.rounded"
+    :badge="props.badges"
+    @click="$emit('buttonClick', props.id)"
+  >
+    <slot></slot>
+  </Button>
 </template>
+
+<script>
+enum eSeverity {
+  Primary,
+  Secondary,
+  Success,
+  Info,
+  Warning,
+  Help,
+  Danger
+}
+
+enum eSize {
+  Small,
+  Normal,
+  Large
+}
+
+</script>
 
 <script lang="ts" setup>
 import Button from "primevue/button";
-import { computed } from "vue";
+import { computed, PropType } from "vue";
 
-const emit = defineEmits(['buttonClick']);
+const emit = defineEmits(["buttonClick"]);
 
 const props = defineProps({
-    id: {
-        type: String,
-        required: true
-    },
-    label: {
-        type: String,
-        required: false,
-    },
-    severity: {
-        type: String,
-        default: null,
-    },
-    size: {
-        type: String,
-        default: undefined,
-    },
-    link: {
-        type: String,
-        default: null,
-    },
-    icon: {
-        type: String,
-        default: null,
-    },
-    disabled: {
-        type: Boolean,
-        default: false,
-    },
-    rounded: {
-        type: Boolean,
-        default: true,
-    },
-    badges: {
-        type: String,
-        default: null,
-    },
+  id: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: false,
+  },
+  severity: {
+    type: Number as PropType<eSeverity>,
+    default: eSeverity.Primary,
+  },
+  size: {
+    type: Number as PropType<eSize>,
+    default: eSize.Normal,
+  },
+  link: {
+    type: String,
+    default: null,
+  },
+  icon: {
+    type: String,
+    default: null,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  rounded: {
+    type: Boolean,
+    default: true,
+  },
+  badges: {
+    type: String,
+    default: null,
+  },
 });
 
-const sizeProxy= computed(() => {
-    if(props.size === "normal"){
-        return null;
-    }
-    return props.size as "small" | "large";
+const severityProxy = computed((): string | undefined => {
+  return props.severity === eSeverity.Primary ? undefined : eSeverity[props.severity];
 })
 
+const sizeProxy = computed((): "small" | "large" | undefined => {
+  return props.size === eSize.Normal ? undefined : eSize[props.size] as "small" | "large";
+});
 </script>
