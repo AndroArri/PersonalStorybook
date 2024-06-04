b81a557ff67d0d32fb057b15816fc62fa5ea2811
Andrea Arrighini <andrea.arrighini@ncoreplat.com>
2024-06-04 14:31:04 +0200

    Add addon vue-slots

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,12 +3,13 @@ import type { StorybookConfig } from "@storybook/vue3-vite";
 const config: StorybookConfig = {
   stories: [
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../stories/**/*.stories.@(j|sjsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    'storybook-addon-vue-slots'
   ],
   framework: {
     name: "@storybook/vue3-vite",
