939c498683906a5a574254f4b24c928c097ae78f
Andrea Arrighini <andrea.arrighini@ncoreplat.com>
2024-06-05 08:28:07 +0200

    Add enum Service

---
+++ resources/common/service/EnumService.ts
@@ -0,0 +1,20 @@
+export default class enumService {
+    static of<T extends object>(e: T) {
+        const values = Object.values(e);
+        const keys = Object.keys(e);
+        return {
+            next: <K extends keyof T>(v: T[K]): string | number => values[(values.indexOf(v) + 1) % values.length],
+            current: <K extends keyof T>(v: T[K]): string | number => values[(values.indexOf(v))],
+            toOptions: (): { name: string; value: string }[] => {
+                let result: { name: string; value: string }[] = [];
+                values.forEach(value => {
+                    result.push({
+                        name: keys[values.indexOf(value)],
+                        value: value
+                    })
+                });
+                return result;
+            }
+        }
+    }
+}
