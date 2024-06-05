04126a7e21a0645a58c85d6d69d02034ade656d6
Andrea Arrighini <andrea.arrighini@ncoreplat.com>
2024-06-05 08:29:32 +0200

    Add textarea component

---
+++ resources/js/components/form/Textarea.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="grid grid-cols-1">
+    <label :for="props.id"><slot></slot></label>
+    <Textarea autoResize></Textarea>
+  </div>
+</template>
+
+<script lang="ts">
+export interface iTextareaProps {
+  id: string;
+  rows?: string;
+  cols?: string;
+  invalid?: boolean;
+  disabled?: boolean;
+}
+</script>
+
+<script setup lang="ts">
+import Textarea from "primevue/textarea";
+
+const props = withDefaults(defineProps<iTextareaProps>(), {
+    rows: "5",
+    cols: "30",
+    invalid: false,
+    disabled: false
+});
+</script>
---
+++ stories/components/Form/Textarea.stories.ts
@@ -0,0 +1,34 @@
+
+import Textarea, { itextAreaProps } from "@/components/form/Textarea.vue";
+
+const slotValue = "slotValue";
+const textareaProps: itextAreaProps = {
+    id: "inputswitch",
+}
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+export default {
+    component: Textarea,
+    tags: ["autodocs"],
+    parameters: {
+        // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+        layout: "fullscreen",
+        slots: {
+            default: {
+                description: "Slot di default",
+                template: `<span> ${slotValue} </span>`
+            }
+        }
+    },
+    args: textareaProps
+};
+
+export const Default = {
+    parameters: {
+        slots: {
+            default: {
+                template: "Default"
+            }
+        }
+    }
+};
