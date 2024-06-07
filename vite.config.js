3d229763554256d73c26abdda158739ce522d95c
Andrea Arrighini <andrea.arrighini@ncoreplat.com>
2024-06-07 12:14:26 +0200

    Add vite-plugin-node-polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
 export default defineConfig({
-    plugins: [
-        vue(),
-    ],
-    resolve: {
-        alias: {
-            '@': path.join(__dirname, '/resources/js/'),
-            'resources': path.join(__dirname, '/resources/')
-        }
-    }
+  plugins: [vue(), nodePolyfills()],
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "/resources/js/"),
+      resources: path.join(__dirname, "/resources/"),
+    },
+  },
 });
