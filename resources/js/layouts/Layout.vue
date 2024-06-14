0aa2d5d7d353564cf432f72782c63072ac2ddb16
Andrea Arrighini <andrea.arrighini@ncoreplat.com>
2024-06-14 09:15:53 +0200

    Add defaults to layout

--- resources/js/layouts/Layout.vue
+++ resources/js/layouts/Layout.vue
@@ -1,26 +1,23 @@
 <template>
-    <Head v-if="$page" :title="title" />
-    <Header :user="user" :profileImg="profileImg"> </Header>
+  <Head v-if="$page" :title="title" />
+  <Header :user="user" :profileImg="profileImg"> </Header>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import Header from "@/components/menu/Header.vue";
 import { Head, usePage } from "@inertiajs/vue3";
 
 const page = usePage();
 
-const props = defineProps({
-    title: {
-        type: String,
-        required: true,
-    },
-    user: {
-        type: Object,
-        default: null,
-    },
-    profileImg: {
-        type: String,
-        default: null,
-    },
-});
+const props = withDefaults(
+  defineProps<{
+    title: string;
+    user?: Object;
+    profileImg?: string;
+  }>(),
+  {
+    user: undefined,
+    profileImg: undefined,
+  }
+);
 </script>
