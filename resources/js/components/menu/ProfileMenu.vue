a4f2306711d06caa2d70b78b0009639399a1524b
Andrea Arrighini <andrea.arrighini@ncoreplat.com>
2024-06-14 09:16:03 +0200

    Refactor profile menu

--- resources/js/components/menu/ProfileMenu.vue
+++ resources/js/components/menu/ProfileMenu.vue
@@ -1,51 +1,50 @@
 <template>
-    <div class="flex justify-center">
-        <Button
-            id="avatar"
-            type="button"
-            @click="toggle"
-            aria-haspopup="true"
-            aria-controls="overlay_tmenu"
-        >
-            <Avatar :image="profileImg"></Avatar>
-        </Button>
-        <TieredMenu ref="menu" id="overlay_tmenu" :model="items" popup />
-    </div>
+  <div class="flex justify-center">
+    <Button
+      id="avatar"
+      type="button"
+      label=""
+      @click="toggle"
+      aria-haspopup="true"
+      aria-controls="overlay_tmenu"
+    >
+      <Avatar :image="profileImg"></Avatar>
+    </Button>
+    <TieredMenu ref="menu" id="overlay_tmenu" :model="items" popup />
+  </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { ref } from "vue";
 import Button from "@/components/button/Button.vue";
 import TieredMenu from "primevue/tieredmenu";
 import Avatar from "primevue/avatar";
 
-const props = defineProps({
-    profileImg: String,
-});
+const props = defineProps<{
+  profileImg: string;
+}>();
 
 const emit = defineEmits(["logout"]);
 
 const menu = ref();
 const items = ref([
-    {
-        label: "Settings",
-        icon: "pi pi-user",
-        command: () => {
-            //TODO settings
-            console.log("settings");
-        }
+  {
+    label: "Settings",
+    icon: "pi pi-user",
+    command: () => {
+      //TODO settings
+      console.log("settings");
     },
-    {
-        label: "Sign Out",
-        icon: "pi pi-sign-out",
-        command: () => {
-            //TODO logout
-            console.log("logout");
-        },
+  },
+  {
+    label: "Sign Out",
+    icon: "pi pi-sign-out",
+    command: () => {
+      //TODO logout
+      console.log("logout");
     },
+  },
 ]);
 
-const toggle = (event) => {
-    menu.value.toggle(event);
-};
+const toggle = (event: Event) => menu.value.toggle(event);
 </script>
