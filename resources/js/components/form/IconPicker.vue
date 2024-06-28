76af6053b15e50c48a7099accd012869e9b9d600
AndroArri <a.arrighini93@gmail.com>
2024-06-28 07:31:26 +0200

    Add IconPicker

--- resources/js/components/form/IconPicker.vue
+++ resources/js/components/form/IconPicker.vue
@@ -1,21 +1,47 @@
+<script lang="ts">
+export interface iIconPicker {
+  id: string;
+  invalid?: boolean;
+  disable?: boolean;
+}
+</script>
+
+<script setup lang="ts">
+import ObjService from "resources/common/service/ObjService";
+import Dropdown, { DropdownChangeEvent } from "primevue/dropdown";
+import { PrimeIcons } from "primevue/api";
+import { iDropdownOptionsMap } from "./Dropdown.vue";
+import { ref } from "vue";
+
+const props = withDefaults(defineProps<iIconPicker>(), {
+  invalid: false,
+  disable: false,
+});
+const model = defineModel();
+const iconPicker = ref(null);
+
+</script>
+
 <template>
   <div class="grid grid-cols-1">
     <label :for="props.id"><slot></slot></label>
     <Dropdown
       filter
       :optionLabel="iDropdownOptionsMap.Name"
-      :optionValue = "iDropdownOptionsMap.Value"
+      :optionValue="iDropdownOptionsMap.Value"
       :invalid="props.invalid"
       :disable="props.disable"
       :id="props.id"
       :options="ObjService.of(PrimeIcons).toOptions()"
       placeholder="Seleziona un icona"
       v-model="model"
+      class="w-full"
+      ref="iconPicker"
     >
       <template #value="slotProps">
         <div v-if="slotProps.value">
-          <i :class="slotProps.value.value"></i>
-          <span class="ml-2">{{ slotProps.value.value }}</span>
+          <i :class="slotProps.value"></i>
+          <span class="ml-2">{{ ObjService.of(PrimeIcons).currentKey(slotProps.value).toLowerCase() }}</span>
         </div>
         <span v-else>
           {{ slotProps.placeholder }}
@@ -28,26 +54,3 @@
     </Dropdown>
   </div>
 </template>
-
-<script lang="ts">
-export interface iIconPicker {
-  id: string;
-  invalid?: boolean;
-  disable?: boolean;
-}
-</script>
-
-<script setup lang="ts">
-import ObjService from "resources/common/service/ObjService";
-import Dropdown from "primevue/dropdown";
-import { PrimeIcons } from "primevue/api";
-import { iDropdownOptionsMap } from "./Dropdown.vue";
-
-const props = withDefaults(defineProps<iIconPicker>(), {
-  invalid: false,
-  disable: false,
-});
-const model = defineModel();
-</script>
-
-<style scoped></style>
